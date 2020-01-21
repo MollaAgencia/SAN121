1f8e7fd9da1f63678c5425804e29c09436a562a8
fasmoreira <felippe10augusto@gmail.com>
2020-01-21 15:22:49 -0300

    Atualizacao site

--- SAN121/SAN121/SAN121/js/script.js
+++ SAN121/SAN121/SAN121/js/script.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
 	// Cadastro
 	$('#btnEnviar').bind('click', function (event) {
-	    event.preventDefault();
-	    if ($("#ipt_nome").val() == "" ||
-	        $("#ipt_email").val() == "" ||
-	        $("#ipt_telefone").val() == "" ||
-	        $("#ipt_cnpj").val() == "" ) {
-	        
-	        Swal.fire('Existem campos que n√£o foram preenchidos.');
-	        return false;
-	    }
+        event.preventDefault();
+        if ($("#ipt_nome").val() == "" ||
+            $("#ipt_email").val() == "" ||
+            $("#ipt_telefone").val() == "" ||
+            $("#ipt_cnpj").val() == "") {
+
+            Swal.fire('Existem campos que n„o foram preenchidos.');
+            return false;
+        }
+
 	    let parametros = {};
-	    parametros.CPF = $('#ipt_nome').val();
-	    parametros.NOME = $('#ipt_email').val();
-	    parametros.EMAIL = $('#ipt_telefone').val();
-	    parametros.TELEFONE = $('#ipt_cnpj').val();
+        parametros.Nome = $('#ipt_nome').val();
+        parametros.Email = $('#ipt_email').val();
+        parametros.Telefone = $('#ipt_telefone').val();
+        parametros.CNPJ = $('#ipt_cnpj').val();
 
 	    $.ajax({
 	        type: 'POST',
-	        url: 'XXXXXXXXXXXXX', 
+            url: 'index.aspx/MTD_CadastroArmazena', 
 	        contentType: 'application/json; charset=utf-8',
 	        dataType: 'json',
 	        data: JSON.stringify(parametros),
@@ -27,22 +27,15 @@ $(document).ready(function(){
 	            $('#btnEnviar').attr('disabled', 'disabled');
 	            $('#btnEnviar').html('<div class="btn text-center py-1"><i class="fa fa-1x fa-spinner fa-spin"></i> Processando...</div>');
 	        },
-	        success: function (returnValue) {
-	            var jsonResult = JSON.parse(returnValue.d);
-	            if (jsonResult == null || jsonResult.length == 0) {
-	                Swal.fire({
-	                    title: 'Erro ao cadastrar!',
-	                    text: 'Tente novamente mais tarde.',
-	                    type: 'error',
-	                })
-	                $('#btnEnviar').removeAttr('disabled', 'disabled');
-	            } else if (jsonResult != "") {
-	                Swal.fire({
-	                    title: 'Cadastro realizado com sucesso!',
-	                    text: 'Realize o seu login',
-	                    type: 'success',
-	                })
-	            }
+            success: function (returnValue) {
+                var jsonResult = JSON.parse(returnValue.d);
+                if (jsonResult.PRP_Status === true) {
+                    $('#btnEnviar').html('<div class="btn text-center py-1">Enviar</div>');
+                    $('#msgFinal').html('<div class="alert-info" style="font-size:18px;font-weight: bold;padding: 8px;"><i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + jsonResult.PRP_Mensagem + '</div>');
+                } else if (jsonResult.PRP_Status === false) {
+                    $('#btnEnviar').html('<div class="btn text-center py-1">Enviar</div>');
+                    $('#msgFinal').html('<div class="alert-info" style="font-size:18px;font-weight: bold;padding: 8px;"><i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ jsonResult.PRP_Mensagem +'</div>');
+                }
 	        }
 	    });
 	});
